1d1d0113c8c4b05d6cb6fc1cb0f98ef00a0a43f6
CommanderChuter <commander.chuter@gmail.com>
2023-03-06 21:58:41 -0700

    import gtwrap, collect files and parse

--- rust/rust_wrapper.py
+++ rust/rust_wrapper.py
@@ -15,14 +15,22 @@ import shutil
 
 from pathlib import Path
 
+import sys
+pwd_dir = Path(__file__).parent.resolve()
+sys.path.append((pwd_dir.parent / "wrap").name)
+from gtwrap import interface_parser as parser
+
 
 class InterfaceModule():
 
     def __init__(self, original:Path, root:Path):
         self.original = original
-        self.name = original.name.strip(".i")
+        self.name = original.name.replace(".i", "")
         self.top = root.name
         self.path = Path(self.top) / original.relative_to(root).parent
+        self.interface = self.path / (self.name+".i")
+        self.cpp = self.path / (self.name+".cpp")
+        self.rust = self.path / (self.name+".rs")
 
     def __repr__(self):
         return "{}::{}\n\t{}\n\t{}".format(self.top, self.name, self.path, self.original)
@@ -33,7 +41,6 @@ class InterfaceModule():
 def main():
 
     ## Set running folder to rust folder
-    pwd_dir = Path(__file__).parent.resolve()
     root_dir = pwd_dir.parent
     gtsam_dir = root_dir / "gtsam"
 
@@ -48,18 +55,22 @@ def main():
             if file.endswith(".i"):
                 module = InterfaceModule(Path(root) / file, gtsam_dir)
                 modules.append(module)
+                print(module)
     
-
-    ## Create file/folder tree
-    for module in modules:
-        module.path.mkdir(parents=True, exist_ok=True)
-        shutil.copy(module.original, module.path)
 
     ## Instaniate Symbol database
 
     ## For each interface file
+    for module in modules:
+
+        ## Create file/folder tree
+        module.path.mkdir(parents=True, exist_ok=True)
+        shutil.copy(module.original, module.path)
 
         ## Parse file
+        with open(module.interface, "r", encoding="UTF-8") as file: #TODO: do we need the UTF-8?
+            parse = parser.Module.parseString(file.read())
+            print(parse)
 
         ## Add to Symbol database
 
