caad8f96771400fc91ac4c9a6ae33b8b197cf5fa
CommanderChuter <commander.chuter@gmail.com>
2023-04-21 22:36:11 -0600

    autocxx needs relative path

--- wrap/gtwrap/rust_wrapper/gtsam_sys/build.rs
+++ wrap/gtwrap/rust_wrapper/gtsam_sys/build.rs
@@ -4,10 +4,13 @@ use autocxx_build::Builder;
 fn main() -> miette::Result<()> {
     // Setup files and dirs
     let build_file = PathBuf::from("src/lib.rs");
-    let gtsam_dir = PathBuf::from("../../../../").canonicalize().unwrap();
+    let gtsam_dir = PathBuf::from("../../../../");//.canonicalize().unwrap();
     let lib_dir = gtsam_dir.join("install/lib");
     let lib_file = "libgtsamDebug.lib";
     let include_dir = gtsam_dir.join("install/include");
+    let file = include_dir.join("gtsam/geometry/Point2.h");
+    dbg!(include_dir.exists());
+    dbg!(file.exists());
     println!("cargo:rustc-link-search=native={}", lib_dir.display());
     println!("cargo:rustc-link-lib=static={}", lib_file);
     let mut builder = Builder::new(&build_file, &[&include_dir]).custom_gendir(PathBuf::from("gen")).build()?;
